98a09ea416593c5f44e0b6e09972ac8148c20856
carlie-gorrondona <carlie.gorrondona@gmail.com>
2022-02-12 18:01:35 -0600

    added delete and put features to code over the weekend

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
+
 document.getElementById("complimentButton").onclick = function () {
     axios.get("http://localhost:4000/api/compliment/")
         .then(function (response) {
@@ -5,8 +6,6 @@ document.getElementById("complimentButton").onclick = function () {
           alert(data);
         });
 };
-
-
 
 document.getElementById("fortuneButton").onclick = function () {
 
@@ -17,45 +16,77 @@ document.getElementById("fortuneButton").onclick = function () {
         })
 }
 
-
-document.getElementById("submit").onclick = function () {
-
-    axios.get("http://localhost:4000/api/todolist/")
-        .then(function (response) {
-            const data = response.data;
-        })
-}
-
+//endpoints and callback
+const callback = ({data:todolist}) => displayLists(todolist);
+const createTodolist = body => axios.post("http://localhost:4000/api/todolist/", body).then(callback);
+const deleteItem = id => axios.delete(`http://localhost:4000/api/todolist/${id}`).then(callback);
+const completedItem = id => axios.put(`http://localhost:4000/api/todolist/${id}`).then(callback);
 
 function submitItem (event) {
+    //clears text box after clicking "Submit"
     event.preventDefault();
 
     let toDoListItem = document.querySelector('#todolist');
 
+    //creates object to store req.body
     let bodyObj = {
         toDoListItem: toDoListItem.value,
     }
     
-    createToDoItem(bodyObj);
+    createTodolist(bodyObj);
 
+    //resets value of toDoListItem to empty string so it can take in the next item
     toDoListItem.value = '';
 }
 
 
+const orderedList = document.querySelector("#todo");
+const compOrderedList = document.querySelector("#complete")
 
-const orderedList = document.querySelector('ol');
 
 const createToDoItem = (item) => {
-
+    //creates new list item in HTML
     const toDoItem = document.createElement('li');
 
+    //adds data to HTML li and includes a completed and delete button
     toDoItem.innerHTML = `<p>${item.toDoListItem}</p>
-    <div class="checkbox">
-    <input type="checkbox"><label>Completed</label>
+    <div class="options">
+    <button onclick="completedItem(${item.id})">Completed</button>
+    <button onclick="deleteItem(${item.id})">Delete</button>
     </div>`;
     orderedList.appendChild(toDoItem);
-
 }
+
+
+function createCompletedItem (item) {
+    //creates new list item in HTML
+    const completedItem = document.createElement('li');
+
+    //adds data to HTML li and includes a delete button
+    completedItem.innerHTML = `<p>${item.toDoListItem}</p>
+    <div class="options">
+    <button onclick="deleteItem(${item.id})">Delete</button>
+    </div>`;
+    compOrderedList.appendChild(completedItem);
+}
+
+
+function displayLists(obj) {
+    //resets data in ordered list to empty string to prevent duplicates
+    orderedList.innerHTML = ``;
+    compOrderedList.innerHTML = ``;
+
+    //loops through object's array of objects and sends each index to createToDoItem function
+    for (let i = 0; i < obj.todo.length; i++) {
+        createToDoItem(obj.todo[i])
+    }
+
+    //loops through object's array of objects and sends each index to createCompletedItem function
+    for (let i = 0; i < obj.complete.length; i++) {
+        createCompletedItem(obj.complete[i])
+    }
+}
+
 
 const form = document.querySelector('form');
 
--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
-
 app.use(cors());
-
 app.use(express.json()); // When we want to be able to accept JSON.
+
+
+let globalID = 1; 
 
 app.get("/api/compliment", (req, res) => {
   const compliments = ["Gee, you're a smart cookie!",
@@ -19,20 +19,67 @@ app.get("/api/compliment", (req, res) => {
   let randomCompliment = compliments[randomIndex];
 
   res.status(200).send(randomCompliment);
-  
 });
 
 app.get('/api/fortune', (req, res) => {
+  //array that holds fortunes
+  const fortune = ["You will pass this assessment", "Today, something good will happen to you", "I see a Web Development job in your future", "A faithful friend is a strong defense", "A person is never too old to learn"];
 
-  const fortune = ["You will pass this assessment", "Today, something good will happen to you", "I see a Web Development job in your future", "A faithful friend is a strong defense", "A person is never to (sic) old to learn"];
-
+  //randomizes index of fortune array to get random phrase
   let randomIndex = Math.floor(Math.random() * fortune.length);
   let randomFortune = fortune[randomIndex];
 
   res.status(200).send(randomFortune);
 })
 
+//object to save an array of to-do list items and completed list items
+let listObj = {
+  todo: [], 
+  complete: [],
+};
 
+app.post('/api/todolist', (req, res) => {
+  //destructures toDoListItem from bodyObj in submitItem function (main.js)
+  let {toDoListItem} = req.body;
+
+  //new object to save new list item to
+  let newItem = {
+    id: globalID,
+    toDoListItem
+  }
+
+  //pushes new list item into listObj.todo
+  listObj.todo.push(newItem);
+  res.status(200).send(listObj);
+  globalID++;
+})
+
+app.delete('/api/todolist/:id/', (req, res) => {
+  //sets variables equal to the id param in the URL
+  let id = +req.params.id;
+
+  //removes item from listObj.todo that equals id from URL; remaining items are saved to listObj.todo
+  listObj.todo = listObj.todo.filter(item => item.id !== id);
+
+  //removes item from listObj.complete; remaining items are saved to listObj.complete
+  listObj.complete = listObj.complete.filter(item => item.id !== id);
+
+  res.status(200).send(listObj);
+})
+
+app.put('/api/todolist/:id', (req, res) => {
+  //sets variable equal to the id param in the URL
+  let id = +req.params.id;
+  
+  //locates completed item in listObj.todo and saves it to listObj.complete
+  let complete = listObj.todo.filter(item => item.id === id);
+  listObj.complete = listObj.complete.concat(complete);
+
+  //removes item from listObj.todo that equals id from URL; remaining items are saved to listObj.todo
+  listObj.todo = listObj.todo.filter(item => item.id !== id);
+
+  res.status(200).send(listObj);
+})
 
 
 app.listen(4000, () => console.log("Server running on 4000"));
